f73c4b203b2fb852bbc3d3a3845ce7c7f2431d82
Mansa Kashyap <manasamannu1990@gmail.com>
2025-02-24 12:42:21 +0530

    pushing the file

---
+++ PycharmProjects/pythonProject2/functions.py
@@ -0,0 +1,18 @@
+class student:
+    x=10
+    def f1(self,name,age,loc):
+        print(name)
+        print(age)
+        print(loc)
+s=student()
+print(s.x)
+s.f1('manasa',21,'gbd')
+
+
+class demo:
+    x=10
+    y=20
+    def m1(self,name,age,loc):
+        print(name)
+        print(age)
+
