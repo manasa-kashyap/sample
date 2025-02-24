64ec61ff257db9f4f3efaa142b1dfe05af26a84a
Mansa Kashyap <manasamannu1990@gmail.com>
2025-02-24 13:02:16 +0530

    made some changes locally

--- PycharmProjects/pythonProject4/scatterplt.py
+++ PycharmProjects/pythonProject4/scatterplt.py
@@ -2,7 +2,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 x_position=[1,2,3,4,6]
-y_position=[2,3,1,4,5]
+y_position=[2,3,1,4,7]
 sns.scatterplot(x=x_position,y=y_position,color='red')
 plt.title("toy counts")
 plt.xlabel("Toy Type")
