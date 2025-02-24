e2854e679e291fdc5ea48c6da139b44deb848d3d
Mansa Kashyap <manasamannu1990@gmail.com>
2025-02-24 12:56:54 +0530

    adding a new file

---
+++ PycharmProjects/pythonProject4/scatterplt.py
@@ -0,0 +1,10 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+x_position=[1,2,3,4,5]
+y_position=[2,3,1,4,5]
+sns.scatterplot(x=x_position,y=y_position,color='red')
+plt.title("toy counts")
+plt.xlabel("Toy Type")
+plt.ylabel("count")
+plt.show()
