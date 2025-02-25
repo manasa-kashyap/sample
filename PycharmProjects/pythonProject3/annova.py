8c45330efe48fe04f49a12177fa1030bfafb7b37
Mansa Kashyap <manasamannu1990@gmail.com>
2025-02-25 13:11:07 +0530

    changes made

---
+++ PycharmProjects/pythonProject3/annova.py
@@ -0,0 +1,19 @@
+import numpy as np
+from scipy import stats
+
+from ttest import p_value
+
+region1=[130,130,125,140,135,128,132]
+region2=[150,160,158,162,155,149,157]
+region3=[170,175,172,180,178,176,174]
+
+f_stat,p_value =stats.f_oneway(region1,region2,region3)
+
+print("F-statistic:",f_stat)
+print("p-value",p_value)
+
+alpha=0.05
+if p_value < alpha:
+    print("at least one group has a significantly different mean")
+else:
+    print("there is no significant different mean")
