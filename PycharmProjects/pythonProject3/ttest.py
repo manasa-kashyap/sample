639278704121fb6922e89848d2ee1f9b619eee81
manasa-kashyap <manasamannu1990@gmail.com>
2025-02-25 12:15:23 +0530

    Update ttest.py

--- PycharmProjects/pythonProject3/ttest.py
+++ PycharmProjects/pythonProject3/ttest.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy import stats
 # sample data
-class1_scores=[76,82,85,90,880,78,100]
+class1_scores=[760,82,85,90,880,78,100]
 class2_scores=[880,91,95,900,92,95,900]
 # perform independent t-test
 t_stat, p_value=stats.ttest_ind(class1_scores,class2_scores)
