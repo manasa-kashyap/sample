c27258f996be156e16b015c18a4c3d800c20de71
manasa-kashyap <manasamannu1990@gmail.com>
2025-02-27 12:44:04 +0530

    Update barplt.py

--- PycharmProjects/pythonProject4/barplt.py
+++ PycharmProjects/pythonProject4/barplt.py
@@ -1,6 +1,6 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-toys=['cars','bikes','balls','train']
+toys=['cars','bikes','balls','metro']
 counts=[5,3,7,2]
 custom_colors=['green','blue','pink','yellow']
 sns.barplot(x=toys,y=counts,palette=custom_colors)
