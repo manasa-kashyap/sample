c689f58808d9e8ec66687c5afe59a5e5cb0a996c
manasa-kashyap <manasamannu1990@gmail.com>
2025-03-04 01:15:38 +0530

    Update leap.py

--- PycharmProjects/python7/leap.py
+++ PycharmProjects/python7/leap.py
@@ -1,5 +1,5 @@
 year=int(input("enter a year:"))
-if (year % 4==0 and year % 100 !=0)or(year % 400 ==0):
+if (year % 4==0 and year % 100 !=0)or(year % 500==0):
     print(f"{year} is a leap year.")
 else:
     print(f"{year} is not a leap year")
